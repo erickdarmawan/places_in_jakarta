dac7cbfd49f180dba4789b7f3658810b16fc7fce
erickdarmawan <erickdarmawan83@gmail.com>
2022-10-06 15:42:55 +0700

    Add getName and call

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 // import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:places_in_jakarta/remote_service.dart';
 import 'package:places_in_jakarta/model/places.dart';
@@ -34,10 +35,8 @@ class _HomePageState extends State<HomePage> {
                       child: Card(
                         child: Container(
                             padding: EdgeInsets.all(10),
-                            // height: 400,
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              // crossAxisAlignment: CrossAxisAlignment.stretch,
                               children: [
                                 getImage(snapshot.data?[index].photo),
                                 SizedBox(
@@ -45,12 +44,7 @@ class _HomePageState extends State<HomePage> {
                                 ),
                                 Align(
                                   alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    snapshot.data![index].name,
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                                  child: getName(snapshot.data![index].name),
                                 ),
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.start,
@@ -59,8 +53,10 @@ class _HomePageState extends State<HomePage> {
                                       child: Row(
                                         crossAxisAlignment:
                                             CrossAxisAlignment.start,
-                                        children: getCategories(
-                                            snapshot.data![index].categories),
+                                        children:
+                                            //Image(image: NetworkImage(snapshot.data![index].iconMap.)),
+                                            getCategories(snapshot
+                                                .data![index].categories),
                                       ),
                                     ),
                                     if (snapshot.data?[index].rating != null)
@@ -88,6 +84,11 @@ class _HomePageState extends State<HomePage> {
     return photo != null
         ? Image(height: 300, image: NetworkImage(photo!.constructImageUrl()))
         : Image.asset('asset/placeholderImage.png', height: 300);
+  }
+
+  Text getName(String name) {
+    return Text(name,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
   }
 
   List<Container> getCategories(List<PlaceCategory> categories) {
